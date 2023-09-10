2a1742934c5c68f4b3f78d47996d8cbbb6ec03f4
delameter <0.delameter@gmail.com>
2023-09-10 18:09:46 +0300

    ⌛ DRAFT: ci/cd debug

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,26 +12,26 @@ import pytest
 from pysuncalc import get_times, get_position, SUNRISE, SUNSET, ZENITH, NADIR, _SUN_TIMES
 from . import assert_close
 
-ts2dt = partial(datetime.fromtimestamp, tz=None)
-ts2dtz = partial(datetime.fromtimestamp, tz=timezone(timedelta(hours=3)))
+ts2dtu = partial(datetime.fromtimestamp, tz=timezone(timedelta(hours=0)))
+ts2dtl = partial(datetime.fromtimestamp, tz=timezone(timedelta(hours=3)))
 
 
 class Test:
     @pytest.mark.parametrize(
         "dt, lat, long, expected",
         [
-            (ts2dtz(1694198261), 55.7558, 37.6172, (2.34878, -0.33646)),
-            (ts2dtz(1694198261), 0.0, 0.0, (1.67233, -0.17137)),
-            (ts2dtz(1694198261), 0.0, -90.0, (2.10061, 1.37185)),
-            (ts2dtz(1694198261), 0.0, 90.0, (-2.10061, -1.37185)),
-            (ts2dtz(1694198261), 90.0, 0.0, (1.74304, 0.10004)),
-            (ts2dtz(1694198261), -90.0, 0.0, (1.39854, -0.10004)),
-            (ts2dtz(1000000000), 55.7558, 37.6172, (-1.99389, -0.16365)),
-            (ts2dtz(1000000000), 0.0, 0.0, (-1.77419, -1.09080)),
-            (ts2dtz(1000000000), 0.0, -90.0, (1.67556, -0.46928)),
-            (ts2dtz(1000000000), 0.0, 90.0, (-1.67556, 0.46928)),
-            (ts2dtz(1000000000), 90.0, 0.0, (-2.67008, 0.09341)),
-            (ts2dtz(1000000000), -90.0, 0.0, (-0.47150, -0.09341)),
+            (ts2dtl(1694198261), 55.7558, 37.6172, (2.34878, -0.33646)),
+            (ts2dtl(1694198261), 0.0, 0.0, (1.67233, -0.17137)),
+            (ts2dtl(1694198261), 0.0, -90.0, (2.10061, 1.37185)),
+            (ts2dtl(1694198261), 0.0, 90.0, (-2.10061, -1.37185)),
+            (ts2dtl(1694198261), 90.0, 0.0, (1.74304, 0.10004)),
+            (ts2dtl(1694198261), -90.0, 0.0, (1.39854, -0.10004)),
+            (ts2dtl(1000000000), 55.7558, 37.6172, (-1.99389, -0.16365)),
+            (ts2dtl(1000000000), 0.0, 0.0, (-1.77419, -1.09080)),
+            (ts2dtl(1000000000), 0.0, -90.0, (1.67556, -0.46928)),
+            (ts2dtl(1000000000), 0.0, 90.0, (-1.67556, 0.46928)),
+            (ts2dtl(1000000000), 90.0, 0.0, (-2.67008, 0.09341)),
+            (ts2dtl(1000000000), -90.0, 0.0, (-0.47150, -0.09341)),
         ],
     )
     def test_get_position(
@@ -42,11 +42,11 @@ class Test:
     @pytest.mark.parametrize(
         "dt, lat, long, expected, extra_names",
         [
-            (ts2dtz(1694202441), 55.7558, 37.6172, {SUNRISE: ts2dt(1694141286)}, []),
-            (ts2dtz(1694202441), 55.7558, 37.6172, {SUNSET: ts2dt(1694189383)}, []),
-            (ts2dtz(1554757485), 56.07, 47.14, {ZENITH: ts2dt(1554800055)}, []),
-            (ts2dtz(1672520400), -90.0, 0.0, {}, [NADIR]),
-            (ts2dtz(1672520400), 90.0, 0.0, {}, [ZENITH]),
+            (ts2dtl(1694202441), 55.7558, 37.6172, {SUNRISE: ts2dtu(1694141286)}, []),
+            (ts2dtl(1694202441), 55.7558, 37.6172, {SUNSET: ts2dtu(1694189383)}, []),
+            (ts2dtl(1554757485), 56.07, 47.14, {ZENITH: ts2dtu(1554800055)}, []),
+            (ts2dtl(1672520400), -90.0, 0.0, {}, [NADIR]),
+            (ts2dtl(1672520400), 90.0, 0.0, {}, [ZENITH]),
         ],
     )
     def test_get_times(
@@ -66,4 +66,4 @@ class Test:
                 yield NADIR
                 yield ZENITH
 
-        assert {*_expected()} == {*get_times(ts2dtz(1694202441), 55.7558, 37.6172).keys()}
+        assert {*_expected()} == {*get_times(ts2dtl(1694202441), 55.7558, 37.6172).keys()}
